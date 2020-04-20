35be9342cdb06e5c38805acfc78292e7e9c0148e
Abdul Khan <akhan17@outlook.com>
2020-04-20 20:11:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
+require 'open-uri'
+require 'json'
+require 'pry'
+
 # Write your code here
+class GetRequester
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    @val_str = uri.open.string
+    @val_str
+
+  end
+
+  def parse_json
+    @val_hash = JSON.parse(@val_str)
+    @val_hash
+    binding.pry
+    
+  end
+
+end
